2eb6ee904dc866fc6112822bd36fee0887fc9def
uniquexcel <uniquexcelf@gmail.com>
2022-08-05 06:42:32 -0700

    This programs helps in storing measurement data

---
+++ my_measurement_data.py
@@ -0,0 +1,28 @@
+
+# This program allows users to store the basic measurement.
+
+name = input("What is your name? ")
+sex = input("Are you a male or female? ")
+print("Hello " + name + ", You are a " + sex)
+print("\n")
+
+print("Welcome to the measurement center.\nKindly fill in the correct data below:\n")
+
+Bust = input("Bust measurement: ")
+Upper_waist = input("Upper Waist measurement: ")
+Lower_Waist = input("Lower Waist measurement: ")
+Hip = input("Hip measurement: ")
+input("Thigh measurement: ")
+input("Knee measurement: ")
+input("Ankle measurement: ")
+input("Shoulder measurement: ")
+input("Under-bust measurement: ")
+input("Shoulder to waist measurement: ")
+input("Blouse length measurement: ")
+input("Trouser length measurement: ")
+input("Sleeve length measurement: ")
+input("Round sleeve measurement: ")
+
+print("Thank you for inputting " + name + "'s measurement\n")
+print("Client name: " + name + "\n" + "Bust measurement: " + Bust + "\n" + "Upper Waist measurement: " + Upper_waist)
+print("Lower Waist measurement: " + Lower_Waist + "\n" + "Hip measurement: " + Hip)
